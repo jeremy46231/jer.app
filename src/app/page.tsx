672acf0ca3e8ac97757f6c1774c90dc139fbe82b
Jeremy Woolley <jeremy@jeremywoolley.com>
2025-06-28 20:46:26 +0300

    Basic next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main>
+        <h1>jer.app</h1>
+        <p>This is a simple Next.js application.</p>
+      </main>
+    </div>
+  )
+}
